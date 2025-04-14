6d59624e5175a02aebe05353c1f41a24209a6345
Lisian Shehu <lisian_sh@icloud.com>
2025-04-14 21:59:28 +0200

    dodan gumb za filtriranje po cijeni i vrijeme, ali jos nije funkcionalan za filtriranje

--- frontend/tripPlanner/src/components/SearchComponent.jsx
+++ frontend/tripPlanner/src/components/SearchComponent.jsx
@@ -3,7 +3,7 @@ import { BACKEND_PORT } from "../config";
 import FlightCard from "./FlightCard"; 
 import BusCard from "./BusCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlane, faBus } from "@fortawesome/free-solid-svg-icons";
+import { faPlane, faBus, faFilter } from "@fortawesome/free-solid-svg-icons";
 
 const SearchComponent = () => {
   // Input field displayed values
@@ -359,50 +359,78 @@ const SearchComponent = () => {
         </div>
 
         {/* Date Range */}
-        <div className="flex flex-col md:flex-row gap-6 mt-6">
-          <div className="relative w-full">
-            <label className="block text-sm font-bold mb-2 text-teal-600">
-              Departure Date
-            </label>
-            <div className="relative">
-              <input
-                type="date"
-                className="bg-teal-50 h-10 w-full px-3 rounded-lg border border-teal-300 focus:ring-2 focus:ring-teal-400 outline-none transition"
-                value={fromDate}
-                onChange={(e) => setFromDate(e.target.value)}
-              />
-            </div>
-          </div>
-
-          {tripType === "roundTrip" && (
+          <div className="flex flex-col md:flex-row gap-6 mt-6">
             <div className="relative w-full">
               <label className="block text-sm font-bold mb-2 text-teal-600">
-                Return Date
+                Departure Date
               </label>
               <div className="relative">
                 <input
-                  type="date"
-                  className="bg-teal-50 h-10 w-full px-3 rounded-lg border border-teal-300 focus:ring-2 focus:ring-teal-400 outline-none transition"
-                  value={toDate}
-                  onChange={(e) => setToDate(e.target.value)}
+            type="date"
+            className="bg-teal-50 h-10 w-full px-3 rounded-lg border border-teal-300 focus:ring-2 focus:ring-teal-400 outline-none transition"
+            value={fromDate}
+            onChange={(e) => setFromDate(e.target.value)}
                 />
               </div>
             </div>
-          )}
-        </div>
 
-        {/* Search Button */}
-        <div className="mt-8 text-center">
-          <button
-            onClick={handleSearch}
-            disabled={isSearching}
-            className={`${
-              isSearching ? "bg-teal-400" : "bg-teal-600 hover:bg-teal-700"
-            } text-white px-6 py-2 rounded-lg font-semibold transition`}
-          >
-            {isSearching ? "Searching..." : "Search"}
-          </button>
-        </div>
+            {tripType === "roundTrip" && (
+              <div className="relative w-full">
+                <label className="block text-sm font-bold mb-2 text-teal-600">
+            Return Date
+                </label>
+                <div className="relative">
+            <input
+              type="date"
+              className="bg-teal-50 h-10 w-full px-3 rounded-lg border border-teal-300 focus:ring-2 focus:ring-teal-400 outline-none transition"
+              value={toDate}
+              onChange={(e) => setToDate(e.target.value)}
+            />
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div className="mt-8 flex justify-between items-center">
+              {/* Filter button */}
+              <div className="relative">
+                <select
+            className="bg-gray-200 hover:bg-gray-300 text-teal-700 px-5 py-2 rounded-lg font-medium transition cursor-pointer appearance-none pr-8"
+                >
+             <option value="" selected>
+                Filter by
+             </option>
+            <option value="price">Price</option>
+            <option value="time">Time</option>
+                </select>
+                <div className="absolute inset-y-0 right-2 flex items-center pointer-events-none">
+                  {/*Icon for dropdown arrow*/}
+                    <svg
+                      className="w-4 h-4 text-teal-700"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                </div>
+              </div>
+
+              {/* Search button */}
+            <button
+              onClick={handleSearch}
+              disabled={isSearching}
+              className={`${
+                isSearching ? "bg-teal-400" : "bg-teal-600 hover:bg-teal-700"
+              } text-white px-6 py-2 rounded-lg font-semibold transition`}
+            >
+              {isSearching ? "Searching..." : "Search"}
+            </button>
+          </div>
       </div>
 
       {/* Display search results */}
